dffd97706c11d25da923f042268819d8ede8f0c6
Søren Magnusson <soren@magnussons.dk>
2024-01-30 11:07:49 +0100

    minor cleanup

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-// window.onload = function() {
 
-//     console.log('onload')
 
-    window.addEventListener('scroll', () => {
-        console.log('scroll');
-        console.log(window.pageYOffset)
-        console.log(window.scrollY)
-        let parent = document.getElementById('paralax_container');
-        let children = parent.getElementsByTagName('div');
-        for ( let i = 0; i < children.length; i++) {
-            // children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.lenght) + 'px)';
-            
-            children[i].style.transform = 
-                'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+window.addEventListener('scroll', () => {
+    console.log('scroll');
+    console.log(window.pageYOffset)
+    console.log(window.scrollY)
+    let parent = document.getElementById('paralax_container');
+    let children = parent.getElementsByTagName('div');
+    for ( let i = 0; i < children.length; i++) {
+        // children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.lenght) + 'px)';
+        
+        children[i].style.transform = 
+            'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
 
-            console.log( children[i].style.transform )
-        }
-    }, false)
+        console.log( children[i].style.transform )
+    }
+}, false)
 
-// } 
+
 
 console.log('hej');
